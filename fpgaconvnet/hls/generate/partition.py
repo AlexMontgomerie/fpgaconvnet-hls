534ed9bd00058df3acc48303bbebd0cd9fecbb5f
Abdalrahim Naser <112262876+abdalrahimnaser@users.noreply.github.com>
2024-08-19 08:59:34 +0100

    Update partition.py

--- fpgaconvnet/hls/generate/partition.py
+++ fpgaconvnet/hls/generate/partition.py
@@ -320,13 +320,9 @@ class GeneratePartition:
 
     """
     Vivado HLS functions
-    """
-
-    # Note: you must change all vivado_hls commands to vitis_hls to resolve the error (versions > 2019.2):
-    # vitis_hls: command not found 
-
-
-
+    Note: you must change all vivado_hls commands to vitis_hls to resolve the error:
+    vivado_hls: command not found (versions > 2019.2)
+    """    
     def create_vivado_hls_project(self, fpga_part="xc7z045ffg900-2", clk=5):
 
         # check everything is generated
